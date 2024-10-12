c7d91590f58b2c9344195a0a2d7f401f8cfca3de
Ankit Sharma <sharma79726@gmail.com>
2024-10-12 13:40:29 +0530

    .

---
+++ controllers/listings.js
@@ -0,0 +1,58 @@
+const Listing = require("../models/listing.js");
+
+module.exports.index = async (req, res, next) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+};
+
+module.exports.renderNewForm = (req, res) => {
+  res.render("listings/new.ejs");
+};
+
+module.exports.showListing = async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id)
+    .populate({
+      path: "reviews", // Populate reviews
+      populate: {
+        path: "author", // Populate author inside each review
+      },
+    })
+    .populate("owner");
+  if (!listing) {
+    req.flash("error", "Listing not exists");
+    return res.redirect("/listings");
+  }
+  res.render("listings/show.ejs", { listing });
+};
+
+module.exports.createListing = async (req, res, next) => {
+  const newListing = new Listing(req.body.listing);
+  newListing.owner = req.user._id;
+  await newListing.save();
+  req.flash("success", "New Listing Created!");
+  res.redirect("/listings");
+};
+
+module.exports.renderEditForm = async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  if (!listing) {
+    req.flash("error", "Listing not exists");
+    res.redirect("/listings");
+  }
+  res.render("listings/edit.ejs", { listing });
+};
+
+module.exports.updateListing = async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  req.flash("success", "Listing Updated!");
+};
+
+module.exports.destroyListing = async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  req.flash("success", "Listing Deleted!");
+  res.redirect("/listings");
+};
--- routes/review.js
+++ routes/review.js
@@ -35,7 +35,6 @@ router.delete(
   isReviewAuthor,
   wrapAsync(async (req, res) => {
     let { id, reviewId } = req.params;
-
     await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
     req.flash("success", "Review Deleted!");
