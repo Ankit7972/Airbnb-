33ddc4761ba5727d7053c032ab724d0063e858e5
Ankit Sharma <sharma79726@gmail.com>
2024-10-25 12:09:26 +0530

    .

--- routes/listing.js
+++ routes/listing.js
@@ -4,14 +4,17 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const passport = require("passport");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer = require("multer"); // Multer is help backend to uplode image in different fromate
+const { storage, cloudinary } = require("../cloudConfig.js");
+const upload = multer({ storage }); //initailizing Multer
 
 router
   .route("/")
   .get(wrapAsync(listingController.index)) //Index Route
   .post(
     isLoggedIn,
+    upload.single("listing[image]"),
     validateListing,
     wrapAsync(listingController.createListing)
   ); //Create Route
